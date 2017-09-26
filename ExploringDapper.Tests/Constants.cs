634f0caa2139f813c65f69c59be21f2579e65a18
Sai Gudigundla <saip106@msn.com>
2017-09-25 22:22:18 -0400

    created new project added first test using plain old ado.net

---
+++ ExploringDapper.Tests/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace ExploringDapper.Tests
+{
+    public static class Constants
+    {
+        public static string ConnectionString => "data source=localhost;initial catalog=AdventureWorks;persist security info=False;Integrated Security = true;";
+    }
+}
---
+++ ExploringDapper.Tests/when_querying_department_data_using_ado_dotnet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ExploringDapper.Tests
+{
+    [TestFixture]
+    public class when_querying_department_data_using_ado_dotnet
+    {
+        [Test]
+        public void it_should_get_the_data()
+        {
+            var departments = new List<Department>();
+            using (var sqlConnection = new SqlConnection(Constants.ConnectionString))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = "SELECT * FROM [AdventureWorks].[HumanResources].[Department] WHERE [DepartmentId] = 1";
+                    using (var sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            var department = new Department
+                            {
+                                Id = (short) sqlDataReader["DepartmentId"],
+                                Name = (string) sqlDataReader["Name"],
+                                GroupName = (string) sqlDataReader["GroupName"],
+                            };
+                            departments.Add(department);
+                        }
+                    }
+                }
+            }
+
+            departments.Count.Should().Be(1);
+            departments[0].Name.Should().Be("Engineering");
+            departments[0].GroupName.Should().Be("Research and Development");
+        }
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+    }
+}
