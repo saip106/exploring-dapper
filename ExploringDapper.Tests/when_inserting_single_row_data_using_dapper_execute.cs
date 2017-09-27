18b5ca8f5c486efdf8af656583d2a417cdc76a7b
Sai Gudigundla <saip106@msn.com>
2017-09-26 22:49:12 -0400

    added more tests for execute and query multiple

---
+++ ExploringDapper.Tests/when_inserting_single_row_data_using_dapper_execute.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ExploringDapper.Tests
+{
+    [TestFixture]
+    public class when_inserting_single_row_data_using_dapper_execute
+    {
+        private const string CreateTableSql = "CREATE TABLE Unicorn (Id INTEGER, Name NVARCHAR(50))";
+        private const string InsertDataSql = "INSERT INTO Unicorn (Id, Name) VALUES (1, 'foo')";
+        private const string SelectDataSql = "SELECT * FROM Unicorn";
+        private const string DeleteDataSql = "DELETE FROM Unicorn";
+        private const string DropTableDataSql = "DROP TABLE Unicorn";
+
+        [Test]
+        public void it_should_insert_single_row()
+        {
+            IList<dynamic> unicorns;
+            using (var sqlConnection = new SqlConnection(Constants.ConnectionString))
+            {
+                sqlConnection.Execute(CreateTableSql);
+                sqlConnection.Execute(InsertDataSql);
+
+                unicorns = sqlConnection
+                    .Query(SelectDataSql)
+                    .ToArray();
+            }
+
+            unicorns.Count.Should().Be(1);
+            var unicorn = unicorns.ToArray()[0];
+            Assert.AreEqual(unicorn.Id, 1);
+            Assert.AreEqual(unicorn.Name, "foo");
+
+            using (var sqlConnection = new SqlConnection(Constants.ConnectionString))
+            {
+                sqlConnection.Execute(DeleteDataSql);
+
+                unicorns = sqlConnection
+                    .Query(SelectDataSql)
+                    .ToArray();
+            }
+
+            unicorns.Count.Should().Be(0);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            using (var sqlConnection = new SqlConnection(Constants.ConnectionString))
+            {
+                sqlConnection.Execute(DropTableDataSql);
+            }
+        }
+    }
+}
---
+++ ExploringDapper.Tests/when_querying_data_using_dapper_multiple_query.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ExploringDapper.Tests
+{
+    [TestFixture]
+    public class when_querying_for_grid_data_using_dapper_multiple_query
+    {
+        private const string Sql = "SELECT * FROM [HumanResources].[Department] WHERE [DepartmentId] = @DepartmentId;" +
+                                   "SELECT * FROM [Person].[Person] WHERE BusinessEntityID = @PersonId";
+
+        [Test]
+        public void it_should_get_grid_data()
+        {
+            IList<Department> departments;
+            IList<Person> people;
+            using (var sqlConnection = new SqlConnection(Constants.ConnectionString))
+            {
+                var grid = sqlConnection
+                    .QueryMultiple(Sql, new { DepartmentId = 1, PersonId = 5 });
+
+                departments = grid.Read<Department>().ToList();
+                people = grid.Read<Person>().ToList();
+            }
+
+            departments.Count.Should().Be(1);
+            departments[0].Name.Should().Be("Engineering");
+            departments[0].GroupName.Should().Be("Research and Development");
+
+            people.Count.Should().Be(1);
+            people[0].FirstName.Should().Be("Gail");
+            people[0].LastName.Should().Be("Erickson");
+        }
+    }
+}
