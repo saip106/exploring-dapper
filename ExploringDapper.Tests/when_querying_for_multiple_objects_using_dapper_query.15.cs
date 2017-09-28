828bd63be4fa96bd7930cd6fdb9f5003bf9fcfc5
Sai Gudigundla <saip106@msn.com>
2017-09-27 23:11:32 -0400

    added test for querying multiple objects

---
+++ ExploringDapper.Tests/when_querying_for_multiple_objects_using_dapper_query.15.cs
@@ -0,0 +1,54 @@
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ExploringDapper.Tests
+{
+    [TestFixture]
+    public class when_querying_for_multiple_objects_using_dapper_query
+    {
+        private const string Sql = @"SELECT p.ProductID as Id, p.Name, p.ProductNumber, m.ProductModelID as Id, m.Name FROM Production.Product p
+                                    INNER JOIN Production.ProductModel m on m.ProductModelID = p.ProductModelID
+                                    WHERE ProductID = @ProductID";
+
+        [Test]
+        public void it_should_get_department_data()
+        {
+            Product[] products;
+            using (var sqlConnection = new SqlConnection(Constants.ConnectionString))
+            {
+                products = sqlConnection
+                    .Query<Product, ProductModel, Product>(
+                        Sql,
+                        (product, productModel) =>
+                        {
+                            product.ProductModel = productModel;
+                            return product;
+                        },
+                        new { ProductID = 999 })
+                    .ToArray();
+            }
+
+            products.Length.Should().Be(1);
+            products[0].Id.Should().Be(999);
+            products[0].ProductNumber.Should().Be("BK-R19B-52");
+            products[0].ProductModel.Id.Should().Be(31);
+            products[0].ProductModel.Name.Should().Be("Road-750");
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductNumber { get; set; }
+        public ProductModel ProductModel { get; set; }
+    }
+
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
